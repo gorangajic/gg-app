561d9d75d539db88fb5783d783009bb557eb9064
Goran Gajic <fero.null@gmail.com>
2025-05-28 21:58:25 +0200

    Initial Commit

---
+++ GG/GGApp.swift
@@ -0,0 +1,17 @@
+//
+//  GGApp.swift
+//  GG
+//
+//  Created by gole on 28.5.25..
+//
+
+import SwiftUI
+
+@main
+struct GGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GGTests/GGTests.swift
@@ -0,0 +1,17 @@
+//
+//  GGTests.swift
+//  GGTests
+//
+//  Created by gole on 28.5.25..
+//
+
+import Testing
+@testable import GG
+
+struct GGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
